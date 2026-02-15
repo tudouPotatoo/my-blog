a0a4c0aa9332d97a8b4e12526dc91e2095a0b218
tudou <alicelai435@gmail.com>
2026-02-15 11:47:02 +0800

    chore: add placeholder for deployment

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/yutinglai/Developer/my-blog/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/yutinglai/Developer/my-blog/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/yutinglai/Developer/my-blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/yutinglai/Developer/my-blog/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/yutinglai/Developer/my-blog/src/css/custom.css"),
+];
---
+++ build/assets/js/acecf23e.b72dec61.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmy_blog=globalThis.webpackChunkmy_blog||[]).push([[1903],{1912(l){l.exports=JSON.parse('{"blogBasePath":"/my-blog/blog","blogTitle":"Blog","authorsListPath":"/my-blog/blog/authors"}')}}]);
