c21b76ecd541184dd9471b9077de328aacb0ae88
tudou <alicelai435@gmail.com>
2026-04-09 14:48:12 +0800

    chore: add java support for code blocks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -151,6 +151,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
+        additionalLanguages: ['java'],
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
