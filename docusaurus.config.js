3f965d163ff43d4a449a9e206ce147e2ba5f7205
tudou <alicelai435@gmail.com>
2026-04-26 20:46:44 +0800

    fix(config): add bash to prism additional languages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -155,7 +155,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
-        additionalLanguages: ['java'],
+        additionalLanguages: ['java', 'bash', 'properties'],
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
